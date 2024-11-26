5ab2b6c2211c0ce18d7700493ce4fbd3b12562ca
Probstling <clara.probst@outlook.com>
2024-11-26 22:23:09 +0100

    done extract ref transcripts

--- extract.py
+++ extract.py
@@ -2,25 +2,45 @@ from Bio import SeqIO
 import re
 
 #check total number of transcripts in the GENCODE fasta file
-gencode = 'gencode.v47.pc_transcripts.fa'
-sequences = SeqIO.parse(gencode, 'fasta')
-count = 0
-for record in sequences:
-   count += 1
-print("There were " + str(count) + " records in file " + gencode)
+def counting(fasta_file):
+   sequences = SeqIO.parse(fasta_file, 'fasta')
+   count = 0
+   for record in sequences:
+      count += 1
+   print(str(count) + " records are in file " + fasta_file)
 
-#extracting all transcript ID's from the MANE selection
-mane_select = "MANE.GRCh38.v1.4.summary.txt"
-with open(mane_select, 'r') as mane:
-   lines = mane.readlines()
+#extract all transcript entries from gencode which are listet in the MANE selection
+def MANE_select_transcript_IDs():
 
-pattern = r"ENST\d+\.\d+"
-all_transcripts_ID = []
-for line in lines:
-   transcript_ID = re.findall(pattern, line)
-   all_transcripts_ID.extend(transcript_ID)
+   #extracting all transcript ID's from the MANE selection
+   mane_select = "MANE.GRCh38.v1.4.summary.txt"
+   with open(mane_select, 'r') as mane:
+      lines = mane.readlines()
 
-output_file = "MANE_transcript_IDs.txt"
-with open(output_file, 'w') as f:
-   for match in all_transcripts_ID:
-      f.write(str(match) + '\n')
+   #creating regular expression pattern to extract only transcript IDs into the array
+   pattern = r"ENST\d+\.\d+"
+   mane_ids = []
+   for line in lines:
+      transcript_ID = re.findall(pattern, line)
+      mane_ids.extend(transcript_ID)
+
+   print(f"{len(mane_ids) } reference transcripts are listed in the MANE seleciton.")
+
+   #Parsing through gencode fasta file
+   gencode = 'gencode.v47.pc_transcripts.fa'
+   sequences = SeqIO.parse(gencode, 'fasta')
+   output_file = 'ref_transcript_IDs.fa'
+
+   #writing selection into a new fasta file
+   with open (output_file, 'w') as f:
+      for record in sequences:
+         #strip after | and keep first entry of the id (ENST...)
+         transcript_ID = record.id.split('|')[0] 
+         if transcript_ID in mane_ids: 
+            #write matching record to new output fasta file
+            SeqIO.write(record, f, 'fasta')
+
+if __name__ == "__main__":
+   counting('gencode.v47.pc_transcripts.fa')
+   MANE_select_transcript_IDs()
+   counting('ref_transcript_IDs.fa')
--- test.py
+++ test.py
@@ -1,7 +1,3 @@
 from Bio import SeqIO
+import re 
 
-filename = "NC_000913.fasta"
-count = 0
-for record in SeqIO.parse(filename, "fasta"):
-    count = count + 1
-print("There were " + str(count) + " records in file " + filename)
