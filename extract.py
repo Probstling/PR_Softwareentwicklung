be249642f65dad197f2fe1bd7015e6616a2f084d
Probstling <clara.probst@outlook.com>
2024-12-21 17:56:47 +0100

    docstring implemention

--- extract.py
+++ extract.py
@@ -1,46 +1,70 @@
+"""
+Filter Gencode Transcript IDs from MANE select.
+
+Creates a new fasta file as output.
+"""
+
 from Bio import SeqIO
 import re
 
-#check total number of transcripts in the GENCODE fasta file
+
 def counting(fasta_file):
-   sequences = SeqIO.parse(fasta_file, 'fasta')
-   count = 0
-   for record in sequences:
-      count += 1
-   print(str(count) + " records are in file " + fasta_file)
+    """
+    Check total number of transcripts in fasta files.
 
-#extract all transcript entries from gencode which are listet in the MANE selection
+    Args:
+        fasta_file (.fa): all transcripts before and after filtering
+    """
+    sequences = SeqIO.parse(fasta_file, "fasta")
+    count = 0
+    for record in sequences:
+        count += 1
+    print(str(count) + " records are in file " + fasta_file)
+
+
 def MANE_select_transcript_IDs():
+    """
+    Extract transcript entries from gencode matching MANE select.
 
-   #extracting all transcript ID's from the MANE selection
-   mane_select = "MANE.GRCh38.v1.4.summary.txt"
-   with open(mane_select, 'r') as mane:
-      lines = mane.readlines()
+    All reference transcript IDs from MANE select are listed in an array.
+    Entries from Gencode fasta file are listed in a new fasta file
+    if they are listed in the MANE selection.
 
-   #creating regular expression pattern to extract only transcript IDs into the array
-   pattern = r"ENST\d+\.\d+"
-   mane_ids = []
-   for line in lines:
-      transcript_ID = re.findall(pattern, line)
-      mane_ids.extend(transcript_ID)
+    """
+    # extracting all transcript ID's from the MANE selection
+    mane_select = "MANE.GRCh38.v1.4.summary.txt"
+    with open(mane_select, "r") as mane:
+        lines = mane.readlines()
 
-   print(f"{len(mane_ids) } reference transcripts are listed in the MANE seleciton.")
+    # creating regular expression pattern for extracting transcript ID
+    pattern = r"ENST\d+\.\d+"
+    mane_ids = []
+    for line in lines:
+        transcript_ID = re.findall(pattern, line)
+        mane_ids.extend(transcript_ID)
 
-   #Parsing through gencode fasta file
-   gencode = 'gencode.v47.pc_transcripts.fa'
-   sequences = SeqIO.parse(gencode, 'fasta')
-   output_file = 'ref_transcript_IDs.fa'
+    print(
+        f"{len(mane_ids) } IDs listed in MANE select."
+      )
 
-   #writing selection into a new fasta file
-   with open (output_file, 'w') as f:
-      for record in sequences:
-         #strip after | and keep first entry of the id (ENST...)
-         transcript_ID = record.id.split('|')[0] 
-         if transcript_ID in mane_ids: 
-            #write matching record to new output fasta file
-            SeqIO.write(record, f, 'fasta')
+    # Parsing through gencode fasta file
+    gencode = "gencode.v47.pc_transcripts.fa"
+    sequences = SeqIO.parse(gencode, "fasta")
+    output_file = "ref_transcript_IDs.fa"
+
+    # writing selection into a new fasta file
+    with open(output_file, "w") as f:
+        for record in sequences:
+            transcript_ID = record.id.split("|")[0]
+            if transcript_ID in mane_ids:
+                SeqIO.write(record, f, "fasta")
+
 
 if __name__ == "__main__":
-   counting('gencode.v47.pc_transcripts.fa')
-   MANE_select_transcript_IDs()
-   counting('ref_transcript_IDs.fa')
+    """
+    Main script execution.
+    Counts before and after filtering.
+    """
+    counting("gencode.v47.pc_transcripts.fa")
+    MANE_select_transcript_IDs()
+    counting("ref_transcript_IDs.fa")
--- features.py
+++ features.py
@@ -1,64 +1,93 @@
+"""
+Extract sequence-specific properties.
+
+Create a cvs file as output.
+"""
+
 from Bio import SeqIO
 import pandas as pd
 import numpy as np
 
-# Extract transcript IDs and create a pandas DataFrame
+
 def create_dataframe(fasta_file):
+    """
+    Create a pandas Dataframe from the reference transcript IDs.
+
+    Args:
+        fasta_file (.fa): holds all reference Transcript IDs
+    Returns:
+        dataframe: Transcript ID as identifier
+    """
     transcript_ids = []
 
     with open(fasta_file, "r") as file:
         for record in SeqIO.parse(file, "fasta"):
-            transcript_id = record.id.split('|')[0]
+            transcript_id = record.id.split("|")[0]
             transcript_ids.append(transcript_id)
 
     dataframe = pd.DataFrame(transcript_ids, columns=["Transcript ID"])
     return dataframe
 
 
-# Extract sequence specific properties: 
-# Length of entire transcript, 5'utr, cds and 3'utr region:
+# Extract sequence specific properties:
 def length(fasta_file):
+    """
+    Extract the length of four regions.
+
+    Args:
+        fasta_file (.fa): holds all reference Transcript IDs
+    Returns:
+        dictionary: Holds the lengths of all regions coupled to Transcript ID
+    """
     properties = {}
 
     with open(fasta_file, "r") as file:
         for record in SeqIO.parse(file, "fasta"):
-            transcript_id = record.id.split('|')[0]
+            transcript_id = record.id.split("|")[0]
 
             # Length of entire transcript
-            total = int(record.id.split('|')[6])
+            total = int(record.id.split("|")[6])
 
             # Length of 5'UTR region
-            utr5 = 'N/A'
-            if 'UTR5:' in record.id:
-                utr5_header = record.id.split('UTR5:')[1].split('|')[0]
-                start, end = map(int, utr5_header.split('-'))
-                utr5 = end - start +1
-            
+            utr5 = "N/A"
+            if "UTR5:" in record.id:
+                utr5_header = record.id.split("UTR5:")[1].split("|")[0]
+                start, end = map(int, utr5_header.split("-"))
+                utr5 = end - start + 1
+
             # Length of CDS
-            if 'CDS:' in record.id:
-                cds_header = record.id.split('CDS:')[1].split('|')[0]
-                start, end = map(int, cds_header.split('-'))
-                cds = end - start +1
+            if "CDS:" in record.id:
+                cds_header = record.id.split("CDS:")[1].split("|")[0]
+                start, end = map(int, cds_header.split("-"))
+                cds = end - start + 1
 
             # Length of 3' UTR region
-            utr3 = 'N/A'
-            if 'UTR3:' in record.id:
-                utr3_header = record.id.split('UTR3:')[1].split('|')[0]
-                start, end = map(int, utr3_header.split('-'))
-                utr3 = end - start +1
+            utr3 = "N/A"
+            if "UTR3:" in record.id:
+                utr3_header = record.id.split("UTR3:")[1].split("|")[0]
+                start, end = map(int, utr3_header.split("-"))
+                utr3 = end - start + 1
 
             properties[transcript_id] = {
                 "Total Length": total,
                 "5' UTR Length": utr5,
                 "CDS Length": cds,
-                "3' UTR Length": utr3
+                "3' UTR Length": utr3,
             }
-    
+
     return properties
 
 
-# Map properties to the DataFrame according to Transcript ID
 def map_features(dataframe, properties):
+    """
+    Map extracted properties to the DataFrame according to Transcript ID.
+
+    Args:
+        dataframe (pandas Dataframe): holds Transcript IDs as identifieres
+        properties (dictionary): holds properties coupled to transcript ID
+    Returns:
+        dataframe: properties coupled to correct Transcript IDs
+    """
     dataframe["Total Length"] = dataframe["Transcript ID"].map(
         lambda x: properties[x]["Total Length"] if x in properties else np.nan
     )
@@ -74,13 +103,27 @@ def map_features(dataframe, properties):
 
     return dataframe
 
-# Export to CSV file
+
 def export(dataframe, output):
+    """
+    Export Dataframe to CSV file.
+
+    Args:
+        dataframe (pandas Dataframe): holds properties coupled to Transcript ID
+        output (csv file): lists properties to Transcript ID in a csv file
+    """
     dataframe.to_csv(output, index=False)
     print(f"DataFrame exportet to {output}")
 
+
 # Call funcitons
 if __name__ == "__main__":
+    """
+    Main script execution.
+    Extracts sequence specific features,
+    stores it in a pandas Dataframe
+    and creates a cvs file as output for further plotting.
+    """
     input = "ref_transcript_IDs.fa"
     dataframe = create_dataframe(input)
     properties = length(input)
