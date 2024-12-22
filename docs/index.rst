656182e3cd070d8bc52696044be355e79e070711
Probstling <clara.probst@outlook.com>
2024-12-23 00:11:28 +0100

    CG content

--- docs/index.rst
+++ docs/index.rst
@@ -11,7 +11,37 @@ Add your content using ``reStructuredText`` syntax. See the
 documentation for details.
 
 
-.. toctree::
-   :maxdepth: 2
-   :caption: Contents:
+Overview
+--------
 
+This project contains three main Python scripts:
+
+- **extract.py**: Extracts sequence specific properties and lists them in a pandas DataFrame.
+- **figures.py**: Creates violin plots from the extracted data.
+- **features.py**: Maps extracted features to the DataFrame and exports it as a CSV file.
+
+Scripts
+-------
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Scripts:
+
+   extract
+   figures
+   features
+
+Script Details
+--------------
+
+.. literalinclude:: ../extract.py
+   :language: python
+   :caption: "extract.py"
+
+.. literalinclude:: ../figures.py
+   :language: python
+   :caption: "figures.py"
+
+.. literalinclude:: ../features.py
+   :language: python
+   :caption: "features.py"
--- features.py
+++ features.py
@@ -29,7 +29,6 @@ def create_dataframe(fasta_file):
     return dataframe
 
 
-# Extract sequence specific properties:
 def length(fasta_file):
     """
     Extract the length of four regions.
@@ -39,7 +38,7 @@ def length(fasta_file):
     Returns:
         dictionary: Holds the lengths of all regions coupled to Transcript ID
     """
-    properties = {}
+    length_properties = {}
 
     with open(fasta_file, "r") as file:
         for record in SeqIO.parse(file, "fasta"):
@@ -68,38 +67,109 @@ def length(fasta_file):
                 start, end = map(int, utr3_header.split("-"))
                 utr3 = end - start + 1
 
-            properties[transcript_id] = {
+            length_properties[transcript_id] = {
                 "Total Length": total,
                 "5' UTR Length": utr5,
                 "CDS Length": cds,
                 "3' UTR Length": utr3,
             }
 
-    return properties
+    return length_properties
 
 
-def map_features(dataframe, properties):
+def cg_content(fasta_file):
+
+    cg_properties = {}
+
+    with open(fasta_file, "r") as file:
+        for record in SeqIO.parse(file, "fasta"):
+            transcript_id = record.id.split("|")[0]
+            sequence = str(record.seq)
+
+            # Function to calculate CG content for each region
+            def calculate_cg_content(seq_slice):
+                if seq_slice:
+                    cg_count = seq_slice.count("C") + seq_slice.count("G")
+                    return round(cg_count / len(seq_slice), 4)
+                return "N/A"
+            
+            # Entire transcript
+            total_content = calculate_cg_content(sequence)            
+
+            # 5' UTR
+            if "UTR5:" in record.id:
+                utr5_header = record.id.split("UTR5:")[1].split("|")[0]
+                start, end = map(int, utr5_header.split("-"))
+                utr5_sequence = sequence[start -1:end]
+                utr5_content = calculate_cg_content(utr5_sequence)
+            else:
+                utr5_content = "N/A"
+
+            # CDS
+            cds_header = record.id.split("CDS:")[1]. split("|")[0]
+            start, end = map(int, cds_header.split("-"))
+            cds_sequence = sequence[start -1:end]
+            cds_content = calculate_cg_content(cds_sequence)
+
+            # 3' UTR 
+            if "UTR3:" in record.id:
+                utr3_header = record.id.split("UTR3:")[1].split("|")[0]
+                start, end = map(int, utr3_header.split("-"))
+                utr3_sequence = sequence[start -1:end]
+                utr3_content = calculate_cg_content(utr3_sequence)
+            else:
+                utr3_content = "N/A"
+
+            # Add results to dictionary
+            cg_properties[transcript_id] = {
+                "Total CG Content": total_content,
+                "5' UTR CG Content": utr5_content,
+                "CDS CG Content": cds_content,
+                "3' UTR CG Content": utr3_content,
+            }
+
+    return cg_properties
+
+
+def map_features(dataframe, length_properties, cg_properties):
     """
     Map extracted properties to the DataFrame according to Transcript ID.
 
     Args:
         dataframe (pandas Dataframe): holds Transcript IDs as identifieres
-        properties (dictionary): holds properties coupled to transcript ID
+        length_properties (dictionary): holds length coupled to transcript ID
+        cg_properties (dictionary): holds CG content coupled to transcript ID
     Returns:
         dataframe: properties coupled to correct Transcript IDs
     """
+    # Mapping of Length
     dataframe["Total Length"] = dataframe["Transcript ID"].map(
-        lambda x: properties[x]["Total Length"] if x in properties else np.nan
+        lambda x: length_properties[x]["Total Length"] if x in length_properties else np.nan
     )
     dataframe["5' UTR Length"] = dataframe["Transcript ID"].map(
-        lambda x: properties[x]["5' UTR Length"] if x in properties else np.nan
+        lambda x: length_properties[x]["5' UTR Length"] if x in length_properties else np.nan
     )
     dataframe["CDS Length"] = dataframe["Transcript ID"].map(
-        lambda x: properties[x]["CDS Length"] if x in properties else np.nan
+        lambda x: length_properties[x]["CDS Length"] if x in length_properties else np.nan
     )
     dataframe["3' UTR Length"] = dataframe["Transcript ID"].map(
-        lambda x: properties[x]["3' UTR Length"] if x in properties else np.nan
+        lambda x: length_properties[x]["3' UTR Length"] if x in length_properties else np.nan
     )
+
+    # Mapping of CG content
+    dataframe["Total CG Content"] = dataframe["Transcript ID"].map(
+        lambda x: cg_properties[x]["Total CG Content"] if x in cg_properties else np.nan
+    )
+    dataframe["5' UTR CG Content"] = dataframe["Transcript ID"].map(
+        lambda x: cg_properties[x]["5' UTR CG Content"] if x in cg_properties else np.nan
+    )
+    dataframe["CDS CG Content"] = dataframe["Transcript ID"].map(
+        lambda x: cg_properties[x]["CDS CG Content"] if x in cg_properties else np.nan
+    )
+    dataframe["3' UTR CG Content"] = dataframe["Transcript ID"].map(
+        lambda x: cg_properties[x]["3' UTR CG Content"] if x in cg_properties else np.nan
+    )
+
 
     return dataframe
 
@@ -116,7 +186,6 @@ def export(dataframe, output):
     print(f"DataFrame exportet to {output}")
 
 
-# Call funcitons
 if __name__ == "__main__":
     """
     Main script execution.
@@ -126,6 +195,10 @@ if __name__ == "__main__":
     """
     input = "ref_transcript_IDs.fa"
     dataframe = create_dataframe(input)
-    properties = length(input)
-    dataframe = map_features(dataframe, properties)
+
+    length_properties = length(input)
+    cg_properties = cg_content(input)
+
+    dataframe = map_features(dataframe, length_properties, cg_properties)
+
     export(dataframe, "output.csv")
